4ff16957bacd2fd86e9e2b52e04031a15e9692f8
Максим Филатов <swapper1983@yandex.ru>
2024-04-27 16:34:37 +0300

    Add deploy. v1.12

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py migrate
 
 python manage.py collectstatic --no-input
 python manage.py fill_db
-gunicorn foodgram.wsgi:application --bind 0.0.0.0:8750
+gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
