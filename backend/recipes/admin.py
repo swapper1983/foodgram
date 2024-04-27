87756e25b172532d891792114872c60e4ad2a2a8
Максим Филатов <swapper1983@yandex.ru>
2024-04-27 23:58:42 +0300

    Add deploy. v1.23

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,70 +1,70 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import (Favorites, Ingredient, Recipe, RecipeIngredient,
-#                      ShoppingCart, Subscriptions, Tag)
-
-
-# class RecipeIngredientInline(admin.TabularInline):
-#     model = RecipeIngredient
-#     extra = 1
+from .models import (Favorites, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Subscriptions, Tag)
 
 
-# @admin.register(Recipe)
-# class RecipeAdmin(admin.ModelAdmin):
-#     inlines = [RecipeIngredientInline, ]
-#     list_display = ('name', 'author_name', 'cooking_time', 'favorites_count')
-#     search_fields = ['name', 'text', 'author__username']
-#     list_filter = ('tags', 'author')
-
-#     def author_name(self, obj):
-#         return obj.author.first_name + ' ' + obj.author.last_name
-#     author_name.short_description = 'Имя и фамилия автора'
-
-#     def favorites_count(self, obj):
-#         return Favorites.objects.filter(recipes=obj).count()
-#     favorites_count.short_description = 'Добавлений в избранное'
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
 
 
-# @admin.register(Ingredient)
-# class IngredientAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'measurement_unit')
-#     search_fields = ['name']
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline, ]
+    list_display = ('name', 'author_name', 'cooking_time', 'favorites_count')
+    search_fields = ['name', 'text', 'author__username']
+    list_filter = ('tags', 'author')
+
+    def author_name(self, obj):
+        return obj.author.first_name + ' ' + obj.author.last_name
+    author_name.short_description = 'Имя и фамилия автора'
+
+    def favorites_count(self, obj):
+        return Favorites.objects.filter(recipes=obj).count()
+    favorites_count.short_description = 'Добавлений в избранное'
 
 
-# @admin.register(Tag)
-# class TagAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'color', 'slug')
-#     search_fields = ['name']
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ['name']
 
 
-# @admin.register(Favorites)
-# class FavoriteAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'list_recipes')
-#     search_fields = ['user__username', 'recipe__name']
-
-#     def list_recipes(self, obj):
-#         return ", ".join([recipe.name for recipe in obj.recipes.all()])
-
-#     list_recipes.short_description = 'Recipes'
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    search_fields = ['name']
 
 
-# @admin.register(ShoppingCart)
-# class ShoppingCartAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'list_recipes')
-#     search_fields = ['user__username', 'recipe__name']
+@admin.register(Favorites)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'list_recipes')
+    search_fields = ['user__username', 'recipe__name']
 
-#     def list_recipes(self, obj):
-#         return ", ".join([recipe.name for recipe in obj.recipes.all()])
+    def list_recipes(self, obj):
+        return ", ".join([recipe.name for recipe in obj.recipes.all()])
 
-#     list_recipes.short_description = 'Recipes'
+    list_recipes.short_description = 'Recipes'
 
 
-# @admin.register(Subscriptions)
-# class SubscriptionAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'list_subscriptions')
-#     search_fields = ['user__username', 'subscription__username']
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'list_recipes')
+    search_fields = ['user__username', 'recipe__name']
 
-#     def list_subscriptions(self, obj):
-#         return ", ".join([user.username for user in obj.subscription.all()])
+    def list_recipes(self, obj):
+        return ", ".join([recipe.name for recipe in obj.recipes.all()])
 
-#     list_subscriptions.short_description = 'Subscriptions'
+    list_recipes.short_description = 'Recipes'
+
+
+@admin.register(Subscriptions)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'list_subscriptions')
+    search_fields = ['user__username', 'subscription__username']
+
+    def list_subscriptions(self, obj):
+        return ", ".join([user.username for user in obj.subscription.all()])
+
+    list_subscriptions.short_description = 'Subscriptions'
