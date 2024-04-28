ee7c13cfbc013d8b6383631f1c5ad865e118b9ae
Максим Филатов <swapper1983@yandex.ru>
2024-04-28 10:48:28 +0300

    Add deploy. v1.25

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.getenv('DEBUG', False) == 'True'
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['mealgram.duckdns.org']
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
